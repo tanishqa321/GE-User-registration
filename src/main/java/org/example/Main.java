5d8c4d2f82896f00a8df20939ac96eacadfe7a38
tanishqa321 <tender.tanishka@gmail.com>
2025-01-08 03:52:23 +0530

    Added UC2: validating last name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,14 +8,23 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         // Regex pattern for First Name validation (Starts with Capital and at least 3 characters)
-        String firstNamePattern = "^[A-Z][a-zA-Z]{2,}$";
+        String namePattern = "^[A-Z][a-zA-Z]{2,}$";
 
         System.out.println("Please enter your first name:");
         String firstName = scanner.nextLine();
-        if (Pattern.matches(firstNamePattern, firstName)) {
+        if (Pattern.matches(namePattern, firstName)) {
             System.out.println("Valid first name: " + firstName);
         } else {
             System.out.println("Invalid first name. Please ensure it starts with a capital letter and has at least 3 characters.");
+        }
+        System.out.println("Please enter your last name:");
+        String lastName = scanner.nextLine();
+
+        //UC2:  Validate last name
+        if (Pattern.matches(namePattern, lastName)) {
+            System.out.println("Valid last name: " + lastName);
+        } else {
+            System.out.println("Invalid last name. Please ensure it starts with a capital letter and has at least 3 characters.");
         }
         scanner.close();
     }
